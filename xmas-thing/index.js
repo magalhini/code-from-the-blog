6e7234b08876d02b20d6be0f890d03d130029a6d
Ricardo Magalhães <magalhini@gmail.com>
2016-12-08 16:44:14 +0100

    Update Xmas project

--- xmas-thing/index.js
+++ xmas-thing/index.js
@@ -2,6 +2,7 @@ const five = require('johnny-five');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 const port = process.env.PORT || 3000;
 
@@ -9,6 +10,20 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 });
+
+const defaultColor = '#d8db9c';
+
+io.on('connection', (client) => {
+  client.emit('connected', defaultColor);
+  client.on('join', handshake => console.log(handshake));
+
+  client.on('rgb', (color) => {
+    console.log('new color', color);
+    client.emit('rgb', color);
+  })
+});
+
+// now: https://xmas-thing-inhtjyfibs.now.sh/
 
 server.listen(port);
 console.log(`Server listening on http://localhost:${port}`);
--- xmas-thing/public/client.js
+++ xmas-thing/public/client.js
@@ -1,11 +1,45 @@
 $(document).ready(function() {
+  var socket = io.connect(window.location.hostname + ':' + 3000);
+  var isConnected = false;
+  var statusElement = $('.status');
+  var houseElement = $('.house');
+
+  var setHouseColour = function(colour) {
+    houseElement.css('fill', colour);
+  };
+
+  var emitNewValue = function(value) {
+    socket.emit('rgb', {
+      value: value
+    });
+  };
+
+  socket.on('rgb', function(data) {
+    console.log('data is', data);
+  });
+
+  setTimeout(function() {
+    if (!isConnected) statusElement.fadeIn();
+  }, 5000);
+
+  socket.on('connected', function(data) {
+    houseElement.css('fill', data);
+    statusElement.hide();
+    isConnected = true;
+  });
+
+  socket.on('connect', function(data) {
+    socket.emit('join', 'Client is connected!');
+  });
+
   $('#picker').colpick({
     layout: 'RGB',
     onSubmit: function(hsb, hex, rgb, el) {
       $(el).colpickHide();
     },
     onChange:function(hsb,hex,rgb,el,bySetColor) {
-      $(el).children().css('background', '#' + hex);
+      $('.house').css('fill', '#' + hex);
+      emitNewValue(rgb);
     }
   });
 });
